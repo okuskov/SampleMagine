91b1028dad2c6b2fc3a6cda07cf9762a00c4f1d6
Kuskov Alex <okuskov@remit.se>
2017-06-01 15:06:56 +0300

    video playback feature

--- app/src/main/java/com/magine/sample/view/VideoPlayerActivity.java
+++ app/src/main/java/com/magine/sample/view/VideoPlayerActivity.java
@@ -1,11 +1,17 @@
 package com.magine.sample.view;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.widget.VideoView;
+
+import com.magine.sample.R;
 
 /**
  * Video player fullscreen activity
+ *
+ * TODO: we also can add UI controls (play, pause, stop) and pause video while changing phone orientation
  */
 public class VideoPlayerActivity extends AppCompatActivity {
 
@@ -14,9 +20,31 @@ public class VideoPlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_videoplayer);
+        setContentView(R.layout.activity_videoplayer);
 
         String source = getIntent().getStringExtra(VIDEO_SOURCE);
-        Log.i("foo", "source: " + source);
+        if (source == null) {
+            // TODO: we also can send back message through onActivityResult()
+            finish();
+            return;
+        }
+        setupVideoPlayer(Uri.parse(source));
+    }
+
+    /**
+     * Setup and play video on screen
+     * @param source Uri
+     */
+    private void setupVideoPlayer(Uri source) {
+        final VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoURI(source);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                videoView.start();
+            }
+        });
     }
 }
